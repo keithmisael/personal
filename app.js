582caa307b1ecb3f6319d02f970497322b0bf127
Keith Misael <95721992+keithmisael@users.noreply.github.com>
2022-06-20 01:12:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -38,8 +38,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000)
+const port = process.env.PORT || 3000;
 
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
 
 
 module.exports = app;
